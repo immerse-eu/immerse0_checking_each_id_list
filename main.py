1e87d6f5e1a5d849bc6d1b6dbe8584119c175958
NAS-Ragde <nayeli.anaid.silva@gmail.com>
2025-05-27 16:08:25 +0200

    refactor: clean up code structure

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ import idlist4pseudonimization
 import os
 import yaml
 
-
 # Read configuration file
-with open("config.yaml", "r", encoding="utf-8") as f:
+with open("../assets/config.yaml", "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
 # Get paths from config
@@ -23,53 +22,136 @@ save_path_IDpseudo = config['extractedIDsPath']['save_path_IDpseudo']
 
 output_filename_maganamed = "extracted_ids_maganamed.csv"
 output_filename_dmmh = "extracted_ids_dmmh.csv"
-output_filename_moviESM = "extracted_ids_movisensESM.csv"
-output_filename_moviSensing = "extracted_ids_movisensSensing.csv"
+output_filename_moviESM = "extracted_ids_movisens_esm.csv"
+output_filename_moviSensing = "extracted_ids_movisens_sensing.csv"
+
+concatenated_redcap = config['redcap_master_path']['all_ids_redcap']
+save_path_missingIDs = config['extractedIDsPath']['save_path_missingIDs']
 
 
-# Extract IDs from various Data sources
-# (1) extract IDs from MaganaMed (! inclusive Clinician IDs, Admin IDs, ..., and so on.)
-maganamedIDs_df = prepare_maganamed_idlist.collect_unique_id_maganamed(base_path_maganamed, save_path_extractedIDs, output_filename_maganamed)
-prepare_maganamed_idlist.analyze_and_categorize_ids(save_path_extractedIDs, output_filename_maganamed, save_path_extractedIDs)
+def run_data_extraction():
+    # (1) extract IDs from MaganaMed.
+    raw_df_maganamed = prepare_maganamed_idlist.collect_unique_id_maganamed(
+        base_path_maganamed,
+        save_path_extractedIDs,
+        output_filename_maganamed
+    )
 
-# (2) extract IDs from DMMH
-# Please choose a proper function:
-    # If we received one single JSON file per center containing data across all dates
-# dmmhIDs_df = prepare_dmmh_idlist.collect_unique_id_dmmh_singleExport(base_path_dmmh, save_path_extractedIDs, output_filename_dmmh)
-    # If we received multiple JSON files per center, separated by date
-dmmhIDs_df = prepare_dmmh_idlist.collect_unique_id_dmmh_multipleExport(base_path_dmmh, save_path_extractedIDs, output_filename_dmmh)
-prepare_dmmh_idlist.analyze_and_categorize_ids(save_path_extractedIDs, output_filename_dmmh, save_path_extractedIDs)
+    # (2) extract IDs from DMMH.
+    raw_df_dmmh = prepare_dmmh_idlist.collect_unique_id_dmmh_multiple_export(
+        base_path_dmmh,
+        save_path_extractedIDs,
+        output_filename_dmmh
+    )
 
-# (3) extract IDs from movisensESM
-movisensESMIDs_df = prepare_movisens_idlist.collect_unique_id_movisensESM(base_path_movisens, save_path_extractedIDs, output_filename_moviESM)
-prepare_movisens_idlist.analyze_and_categorize_ids_ESM(save_path_extractedIDs, output_filename_moviESM, save_path_extractedIDs)
+    # (3) extract IDs from movisensESM
+    raw_df_movisens_esm = prepare_movisens_idlist.collect_unique_id_movisens_esm(
+        base_path_movisens,
+        save_path_extractedIDs,
+        output_filename_moviESM
+    )
 
-# (4) extract IDs from movisensSensing
-movisensSensingIDs_df = prepare_movisens_idlist.collect_unique_id_movisensSensing(base_path_movisens, save_path_extractedIDs, output_filename_moviSensing)
-prepare_movisens_idlist.analyze_and_categorize_ids_Sensing(save_path_extractedIDs, output_filename_moviSensing, save_path_extractedIDs)
+    # (4) extract IDs from movisensSensing
+    raw_df_movisens_sensing = prepare_movisens_idlist.collect_unique_id_movisens_sensing(
+        base_path_movisens,
+        save_path_extractedIDs,
+        output_filename_moviSensing
+    )
 
-# (5) Collect unique ID from all extracted_IDs
-idlist4pseudonimization.collect_unique_id_4_pseudonimizatoin(base_path_IDpseudo, save_path_IDpseudo)
-
-
-
-# Optional: just to control id inconsistencies ----------------------------------------
-# Op1. id list comparison
-# id_inconsistency_check.compare_redcap_maganamed_ids(concatenated_redcap, maganamedIDs_df, save_path_missingIDs) # only PatientIDs (not ClinicianIDs, ..)
-# id_inconsistency_check.compare_redcap_dmmh_ids(concatenated_redcap, dmmhIDs_df, save_path_missingIDs)
-# id_inconsistency_check.compare_redcap_movisensESM_ids(concatenated_redcap, movisensESMIDs_df, save_path_missingIDs)
-# id_inconsistency_check.compare_redcap_movisensSensing_ids(concatenated_redcap, movisensSensingIDs_df, save_path_missingIDs)
-
-# Op2. id list comparison
-# id_inconsistency_check.merge_maganamed_with_redcap(
-#     maganamed_csv_path = os.path.join(list_base_path, "extracted_ids_maganamed.csv"),
-#     redcap_csv_path = os.path.join(list_base_path, "redcap_02_concatenated_modified.csv"),
-#     output_csv_path = os.path.join(list_base_path, "00_merged_magana_redcap.csv"),
-#     missing_in_redcap_log_path=os.path.join(list_base_path, "missing_in_redcap.csv"),
-#     missing_in_maganamed_log_path=os.path.join(list_base_path, "missing_in_maganamed.csv")
-# )
+    # (5) Collect unique ID from all extracted_IDs
+    # idlist4pseudonimization.collect_unique_id_4_pseudonimizatoin(
+    #     base_path_IDpseudo,
+    #     save_path_IDpseudo
+    # )
+    return raw_df_maganamed, raw_df_dmmh, raw_df_movisens_esm, raw_df_movisens_sensing
 
 
+def sort_extracted_data():
+    # (1) Categorize IDs from Maganamed
+    prepare_maganamed_idlist.analyze_and_categorize_ids(
+        save_path_extractedIDs,
+        output_filename_maganamed,
+        save_path_extractedIDs
+    )
+
+    # (2) Categorize IDs from DMMH.
+    #  Option 1. If we received one single JSON file per center containing data across all dates
+    # prepare_dmmh_idlist.collect_unique_id_dmmh_singleExport(
+    #     base_path_dmmh,
+    #     save_path_extractedIDs,
+    #     output_filename_dmmh
+    # )
+
+    #  Option 2. If we received multiple JSON files per center, separated by date
+    prepare_dmmh_idlist.analyze_and_categorize_ids(
+        save_path_extractedIDs,
+        output_filename_dmmh,
+        save_path_extractedIDs
+    )
+
+    # (3) Categorize IDs from movisensESM
+    prepare_movisens_idlist.analyze_and_categorize_ids_ESM(
+        save_path_extractedIDs,
+        output_filename_moviESM,
+        save_path_extractedIDs
+    )
+
+    # (4) Categorize IDs from movisensSensing
+    prepare_movisens_idlist.analyze_and_categorize_ids_Sensing(
+        save_path_extractedIDs,
+        output_filename_moviSensing,
+        save_path_extractedIDs
+    )
+
+    #  (5) Collect unique ID from all extracted_IDs
+    # idlist4pseudonimization.collect_unique_id_4_pseudonimizatoin(base_path_IDpseudo, save_path_IDpseudo)
 
 
+def compare_extracted_ids_with_redcap(raw_df_maganamed, raw_df_dmmh, raw_df_movisens_esm, raw_df_movisens_sensing):
+    # Optional: just to control id inconsistencies ----------------------------------------
+    # Op1. id list comparison
+    id_inconsistency_check.compare_redcap_maganamed_ids(
+        concatenated_redcap,
+        raw_df_maganamed,
+        save_path_missingIDs
+    )  # only PatientIDs (not ClinicianIDs, ..)
 
+    id_inconsistency_check.compare_redcap_dmmh_ids(
+        concatenated_redcap,
+        raw_df_dmmh,
+        save_path_missingIDs
+    )
+
+    id_inconsistency_check.compare_redcap_movisensESM_ids(
+        concatenated_redcap,
+        raw_df_movisens_esm,
+        save_path_missingIDs
+    )
+
+    id_inconsistency_check.compare_redcap_movisensSensing_ids(
+        concatenated_redcap,
+        raw_df_movisens_sensing,
+        save_path_missingIDs
+    )
+
+def main():
+    raw_df_maganamed, raw_df_dmmh, raw_df_movisens_esm, raw_df_movisens_sensing = run_data_extraction()
+    sort_extracted_data()
+    compare_extracted_ids_with_redcap(
+        raw_df_maganamed,
+        raw_df_dmmh,
+        raw_df_movisens_esm,
+        raw_df_movisens_sensing
+    )
+
+    # Op2. id list comparison
+    # id_inconsistency_check.merge_maganamed_with_redcap(
+    #     maganamed_csv_path = os.path.join(list_base_path, "extracted_ids_maganamed.csv"),
+    #     redcap_csv_path = os.path.join(list_base_path, "redcap_02_concatenated_modified.csv"),
+    #     output_csv_path = os.path.join(list_base_path, "00_merged_magana_redcap.csv"),
+    #     missing_in_redcap_log_path=os.path.join(list_base_path, "missing_in_redcap.csv"),
+    #     missing_in_maganamed_log_path=os.path.join(list_base_path, "missing_in_maganamed.csv")
+    # )
+
+if __name__ == "__main__":
+    main()
